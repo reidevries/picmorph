5f23db285cbf9c36bb13d7b86b6e714f1f907b0f
Rae Aw <raeaw@localhost.localdomain>
2020-02-11 01:15:33 +1030

    img morph works good

--- linemorph.py
+++ linemorph.py
@@ -1,210 +1,417 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageChops
+from scipy.spatial import Delaunay
+import numpy as np
+import os
 import math
+import subprocess
+import shlex
 
-def open_image(path):
-    newImage = Image.open(path)
-    return newImage
+M_PI = 3.14
+reduce_width = 400
+reduce_height = 300
 
-def save_image(image, path):
-    image.save(path, 'png')
+def getPixel(image, coord):
+	i = coord[0]
+	j = coord[1]
+	width, height = image.size
+	
+	pixel = image.getpixel((max(0,min(width-1,i)),max(0,min(height-1,j))))
+	return pixel
 
-def create_image(i, j):
-    image = Image.new("RGB", (i,j), "white")
-    return image
+def sumRGB(rgb):
+	return rgb[0]+rgb[1]+rgb[2]
 
-def get_pixel(image, i, j):
-    width, height = image.size
-    if i>width or j>height:
-        return None
+def addColors(colora, colorb):	#expects three-element tuples representing the colours
+	newr = max(0, min(1, colora[0]+colorb[0]))
+	newg = max(0, min(1, colora[1]+colorb[1]))
+	newb = max(0, min(1, colora[2]+colorb[2]))
+	return (newr, newg, newb, 255)
 
-    pixel = image.getpixel((i,j))
-    return pixel
+def blendColors(colora, colorb, colorc, pos, pixel_alpha):
+	alpha_pos =  float(pixel_alpha)/255
+	newr = int((colora[0]*(1-pos) + colorb[0]*pos)*alpha_pos + colorc[0]*(1-alpha_pos))
+	newg = int((colora[1]*(1-pos) + colorb[1]*pos)*alpha_pos + colorc[1]*(1-alpha_pos))
+	newb = int((colora[2]*(1-pos) + colorb[2]*pos)*alpha_pos + colorc[2]*(1-alpha_pos))
 
-def sumrgb(rgb):
-    return rgb[0]+rgb[1]+rgb[2]
+	return (newr, newg, newb, 255)
 
-def edgedetect(image):
-    print("applying sobel edge detection...")
-    width,height = image.size
-    pixel = image.load()
-    newimage = create_image(width, height)
-    newpixel = newimage.load()
-    
-    for i in range(width):
-        for j in range(height):
-            il = max(i-1, 0)
-            ir = min(i,width-1)
-            ju = max(j-1, 0)
-            jd = min(j,height-1)
-            
-            tl = sumrgb(pixel[il,ju])
-            t = sumrgb(pixel[i,ju])
-            tr = sumrgb(pixel[ir,ju])
-            l = sumrgb(pixel[il,j])
-            r = sumrgb(pixel[ir,j])
-            bl = sumrgb(pixel[il,jd])
-            b = sumrgb(pixel[i,jd])
-            br = sumrgb(pixel[ir,jd])
-            
-            gx = abs(tr-tl+2*(r-l)+br-bl)
-            gy = abs(tl-bl+2*(t-b)+tr-br)
+def reduceSize(image):
+	print("resizing to " +str(reduce_width)+"x"+str(reduce_height)+"...")
+	return image.resize((reduce_width, reduce_height), resample=Image.BILINEAR)
 
-            g = int(math.sqrt(gx*gx + gy*gy))
-            
-            newpixel[i,j] = (g,g,g)
+def equalSize(a, b):
+	aw, ah = a.size
+	bw, bh = b.size
+	if (bw == aw and bh == ah):
+		a_out = Image.new("RGBA", (bw,bh), (0,0,0,0))
+		a_out.paste(a)
+		b_out = Image.new("RGBA", (bw,bh), (0,0,0,0))
+		b_out.paste(b)
+		return a_out,b_out
 
-    return newimage
+	ow, oh = a.size
+	if (bw > aw):
+		ow = bw
+	if (bh > ah):
+		oh = bh
+	print("resizing both images to " +str(ow)+"x"+str(oh)+"...")
+
+	a_out = Image.new("RGBA", (ow,oh), (0,0,0,0))
+	a_out.paste(a.resize((ow,oh), resample=Image.BILINEAR))
+	b_out = Image.new("RGBA", (ow, oh), (0, 0, 0, 0))
+	b_out.paste(b.resize((ow, oh), resample=Image.BILINEAR))
+	return a_out,b_out
+
+def edgedetect(image, line_width=1):
+	print("applying sobel edge detection...")
+	width,height = image.size
+	pixel = image.load()
+	newimage = Image.new("RGBA", (width, height), "white")
+	newdrawing = ImageDraw.Draw(newimage)
+	
+	for i in range(width):
+		for j in range(height):
+
+			il = max(i-1, 0)
+			ir = min(i,width-1)
+			ju = max(j-1, 0)
+			jd = min(j,height-1)
+			
+			tl = sumRGB(pixel[il,ju])
+			t = sumRGB(pixel[i,ju])
+			tr = sumRGB(pixel[ir,ju])
+			l = sumRGB(pixel[il,j])
+			r = sumRGB(pixel[ir,j])
+			bl = sumRGB(pixel[il,jd])
+			b = sumRGB(pixel[i,jd])
+			br = sumRGB(pixel[ir,jd])
+			
+			gx = abs(tr-tl+2*(r-l)+br-bl)
+			gy = abs(tl-bl+2*(t-b)+tr-br)
+
+			g = int(math.sqrt(gx*gx + gy*gy))
+			
+			if (g > 128):
+				newdrawing.ellipse([(i-line_width, j-line_width), (i+line_width, j+line_width)], fill=(0,0,0))
+
+	return newimage
 
 def twotone(image, split=127):
-    print("applying two tone filter to r,g,b channels with split point", split, "...")
-    width,height = image.size
-    newimage = create_image(width,height)
-    newpixel = newimage.load()
+	print("applying two tone filter to r,g,b channels with split point", split, "...")
+	width,height = image.size
+	newimage = Image.new("RGB", (width, height), "white")
+	newpixel = newimage.load()
 
-    for i in range(width):
-        for j in range(height):
-            pixel = get_pixel(image,i,j)
-            r = 0
-            g = 0
-            b = 0
-            if (pixel[0] > split):
-                r = 255
-            if (pixel[1] > split):
-                g = 255
-            if (pixel[2] > split):
-                b = 255
+	for i in range(width):
+		for j in range(height):
+			pixel = getPixel(image,(i,j))
+			r = 0
+			g = 0
+			b = 0
+			if (pixel[0] > split):
+				r = 255
+			if (pixel[1] > split):
+				g = 255
+			if (pixel[2] > split):
+				b = 255
 
-            newpixel[i,j] = (r,g,b)
+			newpixel[i,j] = (r,g,b)
 
-    return newimage
+	return newimage
 
 def normalise(image):
-    print("normalising...")
-    width,height = image.size
-    newimage = create_image(width,height)
-    newpixel = newimage.load()
-    maxbright = 0
-    minbright = 765
-    
-    for i in range(width):
-        for j in range(height):
-            pixel = get_pixel(image,i,j)
-            maxbright = max(maxbright, sumrgb(pixel))
-            minbright = min(minbright, sumrgb(pixel))
-    
-    if (maxbright > 0):
-        maxbright = 765/maxbright
-    else:
-        maxbright = 255
-    minbright = minbright/3
+	print("normalising...")
+	width,height = image.size
+	newimage = Image.new("RGB", (width, height), "white")
+	newpixel = newimage.load()
+	maxbright = 0
+	minbright = 765
+	
+	for i in range(width):
+		for j in range(height):
+			pixel = getPixel(image,(i,j))
+			maxbright = max(maxbright, sumRGB(pixel))
+			minbright = min(minbright, sumRGB(pixel))
+	
+	if (maxbright > 0):
+		maxbright = 765/maxbright
+	else:
+		maxbright = 255
+	minbright = minbright/3
 
-    for i in range(width):
-        for j in range(height):
-            pixel = get_pixel(image,i,j)
-            newpixel[i,j] = (int(maxbright*(pixel[0]-minbright)), int(maxbright*(pixel[1]-minbright)), int(maxbright*(pixel[2]-minbright)))
+	for i in range(width):
+		for j in range(height):
+			pixel = getPixel(image,(i,j))
+			newpixel[i,j] = (int(maxbright*(pixel[0]-minbright)), int(maxbright*(pixel[1]-minbright)), int(maxbright*(pixel[2]-minbright)))
 
-    return newimage
+	return newimage
 
-def reducePoints(pointlist, size=256):
-    oldsize = len(pointlist)+1
-    newsize = len(pointlist)
-    while (oldsize > newsize):
-        oldsize = newsize
-        delinterval = max(0,len(pointlist)-size)
-        if (delinterval > 0):
-            delinterval = math.floor(len(pointlist)/delinterval)
-            for k in range(len(pointlist)):
-                bdx = 0
-                bdy = 0
-                adx = 0
-                ady = 0
-                if (k > 1 and k < len(pointlist)-1):
-                    bdx = pointlist[k-1][0]-pointlist[k][0]
-                    bdy = pointlist[k-1][1]-pointlist[k][1]
-                    adx = pointlist[k][0]-pointlist[k+1][0]
-                    ady = pointlist[k][1]-pointlist[k+1][1]
-                    if (k%delinterval == 0):
-                        if (((bdx > 0 and adx > 0) or (bdx < 0 and adx < 0)) and ((bdy > 0 and ady > 0) or (bdy < 0 and ady < 0))):
-                            pointlist.remove(pointlist[k])
-        newsize = len(pointlist)
-    return pointlist
+def drawEllipseMask(xy, quality=10):
+	newimage = Image.new("RGBA", (xy[1][0]-xy[0][0], xy[1][1]-xy[0][1]), (0,0,0,0))
+	newdrawing = ImageDraw.Draw(newimage)
+	centre_xy = (xy[1][0]/2 + xy[0][0]/2, xy[1][1]/2 + xy[0][1]/2)
+	for i in range(quality):
+		pos = (i/quality)
+		new_xy = ((centre_xy[0]*(1-pos)+xy[0][0]*pos, centre_xy[1]*(1-pos)+xy[0][1]*pos),
+				  (centre_xy[0]*(1-pos)+xy[1][0]*pos, centre_xy[1]*(1-pos)+xy[1][1]*pos))
+		newdrawing.ellipse(new_xy, fill=(255,255,255,255/10))
 
-def annihilatePoints(pointlist, size=256):
-    oldsize = len(pointlist)+1
-    newsize = len(pointlist)
-    while (oldsize > newsize):
-        oldsize = newsize
-        delinterval = max(0,len(pointlist)-size)
-        if (delinterval > 0):
-            delinterval = math.floor(len(pointlist)/delinterval)
-            for k in range(len(pointlist)):
-                if (k%delinterval == 0 and k < len(pointlist)):
-                    pointlist.remove(pointlist[k])
-        newsize = len(pointlist)
-    return pointlist
+def drawPolygonMask(xy, size):
+	newimage = Image.new("RGBA", size, (0,0,0,0))
+	newdrawing = ImageDraw.Draw(newimage)
+	fake_quad = np.array((xy[0], xy[1], xy[2], xy[2]))	#for some reason PIL only likes quads and not triangles, so I convert it to a fake quad
+	newdrawing.polygon(fake_quad, fill=(255,255,255,255), outline=(128,128,128,128))
+	return newimage
 
-def getPointList(image, listsize=256):
-    print("getting a list of points representing the lines of the object...")
-    print("\t", end=" ")
-    newimage = edgedetect(image.resize((300,300)))
-    print("\t", end=" ")
-    newimage = normalise(newimage)
+def getDelaunay(points):
+	nppoints = np.array(points)
+	return Delaunay(nppoints).points
 
-    print("\t creating point list...")
-    pointlist = []
-    for i in range(300):
-        for j in range(300):                               #if the pixel is part of a "line", then try find the nearest point found so far, and insert it after it in the list
-            if (sumrgb(get_pixel(newimage,i,j)) > 127):
-                nearest = 600
-                nearestk = 0
-                for k in range(len(pointlist)):
-                    dist = i-pointlist[k][0]+j-pointlist[k][1]
-                    if (dist < nearest):
-                         nearest = dist
-                         nearestk = k
-                pointlist.insert(nearestk, (i,j))
+def polygonCrop(image, xy):
+	mask = drawPolygonMask(xy, image.size)
+	new_mask, new_image = equalSize(mask, image)
+	return ImageChops.composite(new_image,image,new_mask)
 
-    print("\t reducing points...")
-    
-    reducePoints(pointlist, listsize)
-    
-    annihilatePoints(pointlist, listsize)
-        
-    return pointlist
+def transformTriangle(image, xy, target_xy):
+	cropped_image = polygonCrop(image, xy)
+	coefficients = (target_xy[0][0], target_xy[0][1], target_xy[1][0], target_xy[1][1],
+					target_xy[2][0], target_xy[2][1], target_xy[2][0], target_xy[2][1])
+	new_image = cropped_image.transform(image.size, Image.PERSPECTIVE, coefficients, resample=Image.BILINEAR)
+	return ImageChops.composite(image, new_image, new_image)
 
 def matchPointLists(a, b):
-    matches = []
-    for i in range(len(a)):
-        nearest = 600
-        nearestj = 0
-        for j in range(len(b)):
-            dist = a[i][0]-b[j][0]+a[i][1]-b[j][1]
-            if (dist < nearest):
-                nearest = dist
-                nearestj = j
-        matches.append((i,j))
-    return matches
+	#find the list with more elements and the one with fewer elements
+	less = a
+	more = b
+	swapped = False
+	if (len(b) < len(a)):
+		less = b
+		more = a
+		swapped = True
 
-#interpolate two sets of point lists, 0 < pos < 1
-def interpolatePointLists(a, b, matches, pos):
-    newlist = a
-    for m in matches:
-        newlist[m[0]] = (a[m[0]][0]*(1-pos) + b[m[1]][0]*pos, a[m[0]][1]*(1-pos) + b[m[1]][1]*pos)
-    return newlist
+	more_matched = []
+	for j in range(len(more)):
+		more_matched.append(-1)	#stores which indices of the larger  have been matched
+	matches = []
+
+	for i in range(len(less)):	#first, go through the smaller array and match every element to something
+		nearest = 1000
+		nearestj = -1
+		for j in range(len(more)):
+			dist = abs(less[i][0]-more[j][0])+abs(less[i][1]-more[j][1])
+			if (more_matched[j] < 0 or dist < more_matched[j]):
+				if (dist < nearest):
+					nearest = dist
+					nearestj = j
+				
+		more_matched[nearestj] = nearest
+		if swapped:
+			matches.append((nearestj, i))
+		else:
+			matches.append((i,nearestj))
+
+	for j in range(len(more)):	#second pass to match all the as-of-yet unmatched elements of 'more'
+		if (more_matched[j] < 0):
+			nearest = 1000
+			nearesti = -1
+			for i in range(len(less)):
+				dist = abs(less[i][0]-more[j][0])+abs(less[i][1]-more[j][1])
+				if (dist < nearest):
+					nearest = dist
+					nearesti = i
+
+			if swapped:
+				matches.append((j, nearesti))
+			else:
+				matches.append((nearesti, j))
+
+	return matches
+
+#interpolate two sets of point lists, 0 < pos < 1. 'matches' is an array of tuples (i,j) where 'i' is an index of a_p and 'j' is an index of b_p
+def interpolatePointLists(a_p, b_p, matches, pos):
+	newlist = []
+	for m in matches:
+		newlist.append((a_p[m[0]][0]*(1-pos) + b_p[m[1]][0]*pos, a_p[m[0]][1]*(1-pos) + b_p[m[1]][1]*pos))
+	return newlist
+
+#similar to above, but work on the pixels of two images.
+def interpolateImageFromPointLists(a, b, a_p, b_p, a_d, b_d, matches, pos):
+	print("interpolating two images based on points...")
+
+	if pos == 0:
+		return a
+	elif pos == 1:
+		return b
+
+	close_to_b = False
+	close_d = a_d
+	close = a
+	if (pos > 0.5):
+		close_to_b = True
+		close_d = b_d
+		close = b
+
+	triangles = getDelaunay(interpolatePointLists(a_p, b_p, matches, pos))
+	base = Image.blend(a, b, pos)
+	new_image = base
+	tri_accumulator = []
+	close_d_accumulator = []
+	counter = 0
+	for i in range(len(triangles)):
+		tri_accumulator.append(triangles[i])
+		if i < len(close_d):
+			close_d_accumulator.append(close_d[i])
+		if i%3 == 2:
+			if (len(close_d_accumulator) == 3 and len(tri_accumulator) == 3):
+				new_triangle = transformTriangle(base, close_d_accumulator, tri_accumulator)
+				blend_pos = math.cos(-3*abs(pos-0.5))	#show the polygon mostly in the middle of the interpolation
+				new_image = ImageChops.blend(new_image, new_triangle, blend_pos)
+				print("created " + str(counter) + "th triangle")
+				counter = counter+1
+			tri_accumulator.clear()
+			if i < len(close_d):
+				close_d_accumulator.clear()
+
+	return new_image
+
+def interpolateImageWithEllipses(a,b,a_p,b_p,matches,pos):
+	print("interpolating two images using ellipses...")
+
+	if pos == 0:
+		return a
+	elif pos == 1:
+		return b
+
+	close_to_b = False
+	close_p = a_p
+	close = a
+	if (pos > 0.5):
+		close_to_b = True
+		close_p = b_p
+		close = b
+
+	base = Image.blend(a,b,pos)
+	points = interpolatePointLists(a_p,b_p,matches,pos)
+	m = matches
+	new_image = Image.new("RGBA", base.size, (0,0,0,0))
+	new_image.paste(base)
+	counter = 0
+	for i in range(len(points)):
+		counter = counter+1
+		coord = points[i]
+		a_p_i = a_p[m[i][0]]
+		b_p_i = b_p[m[i][1]]
+		delta = (b_p_i[0]-a_p_i[0], b_p_i[1]-a_p_i[1])
+		centre = (a_p_i[0]+delta[0]/2, a_p_i[1]+delta[1]/2)
+		perp = math.atan2(delta[1], delta[0])-M_PI/2
+		mag2 = delta[0]*delta[0] + delta[1]*delta[1]
+		perp_edge = (centre[0] + math.cos(perp)*mag2, centre[1] + math.sin(perp)*mag2,
+					 centre[0] - math.cos(perp)*mag2, centre[1] - math.sin(perp)*mag2)
+		cropped = polygonCrop(base, perp_edge)
+		blend_pos = math.cos(-3*abs(pos-0.5))	#show the polygon mostly in the middle of the interpolation
+		new_image = ImageChops.blend(new_image, cropped, blend_pos)
+		print("created ellipse" + str(counter) + "/" + str(len(points)), end="\t")
+	print("DONE")
+	return new_image
+
+def interpolateWithDots(a,b,a_p,b_p,matches,pos):
+	print("interpolating two images using dots...")
+
+	if pos == 0:
+		return a
+	elif pos == 1:
+		return b
+
+	close_to_b = False
+	close_p = a_p
+	close = a
+	if (pos > 0.5):
+		close_to_b = True
+		close_p = b_p
+		close = b
+
+	blend_alpha = min(max(pos*2-0.5,0),1)
+	base = Image.blend(a,b,blend_alpha)
+
+	new_image = Image.new("RGBA", base.size, (0,0,0,0))
+	new_image.paste(base)
+	new_drawing = ImageDraw.Draw(new_image)
+
+	points = interpolatePointLists(a_p,b_p,matches,pos)
+	m = matches
+
+	for i in range(len(points)):
+		a_p_i = a_p[m[i][0]]
+		b_p_i = b_p[m[i][1]]
+		dist2_p = 0
+		if close_to_b:
+			dist2_p = int(abs(b_p_i[0] - points[i][0]) + abs(b_p_i[1] - points[i][1]))
+		else:
+			dist2_p = int(abs(a_p_i[0] - points[i][0]) + abs(a_p_i[1] - points[i][1]))
+
+		for x in range(int(-dist2_p), int(dist2_p+1)):
+			for y in range(int(-dist2_p), int(dist2_p+1)):
+				if (abs(x)+abs(y) <= 2*dist2_p+1):
+					coord = (points[i][0]+x, points[i][1]+y)
+					pixel_alpha = min(8*max(2*float(dist2_p)-float(abs(x)+abs(y)),0),255)
+					a_coord = (a_p_i[0]+x, a_p_i[1]+y)
+					b_coord = (b_p_i[0]+x, b_p_i[1]+y)
+					a_colour = getPixel(a, a_coord)
+					a_colour = (a_colour[0], a_colour[1], a_colour[2], pixel_alpha)
+					b_colour = getPixel(b, b_coord)
+					b_colour = (b_colour[0], b_colour[1], b_colour[2], pixel_alpha)
+					c_colour = getPixel(new_image, coord)
+					new_colour = blendColors(a_colour, b_colour, c_colour, blend_alpha, pixel_alpha)
+					new_drawing.point(coord, fill=new_colour)
+
+		print("drew dot " + str(i) + "/" + str(len(points)) + " of size " + str(dist2_p), end="\t\t", flush=True)
+	return new_image
+
 
 def drawImageFromPoints(pointlist):
-    newimage = create_image(300,300)
-    newpixel = newimage.load()
+	print("saving image drawn from points...")
+	width,height = reduce_width,reduce_height
+	newimage = Image.new("RGB", (width,height), "white")
+	newdrawing = ImageDraw.Draw(newimage)
 
-    for i in range(len(pointlist)):
-        pointa = pointlist[i]
-        if (i < len(pointlist)-1):
-            pointb = pointlist[i+1]
-            for j in range(300):
-                interp = j/300.0
-                x = pointa[0]*(1-interp) + pointb[0]*interp
-                y = pointa[1]*(1-interp) + pointb[1]*interp
-                newpixel[x,y] = (1,1,1)
-    return newimage       
-                
-                
+	for i in range(len(pointlist)):
+		pointa = pointlist[i]
+		x = pointa[0]
+		y = pointa[1]
+		newdrawing.ellipse([(x-1, y-1), (x+1, y+1)], fill=(1,1,1))
+
+	del newdrawing
+
+	return newimage
+
+def getPointsFromAutotrace(image, output_scale=(1,1)):
+	if not os.path.exists("./autotrace_temp"):
+		os.makedirs("./autotrace_temp")
+	print("saving image as .bmp format...")
+	width,height = image.size
+	image.save("./autotrace_temp/input.bmp", "BMP")
+
+	print("starting autotrace to get points from image... (please make sure autotrace is installed)")
+	cmd = "autotrace --centerline --color-count=2 --output-file=./autotrace_temp/output.gnuplot --output-format=gnuplot ./autotrace_temp/input.bmp"
+	args = shlex.split(cmd)
+	subprocess.run(args)
+
+	print("getting autotraced image and converting to a list of points...")
+
+	pointlist = []
+	plot = open("./autotrace_temp/output.gnuplot", "r")
+	plotlines = plot.readlines()
+	for p in plotlines:
+		if p[0].isdigit():
+			twostrings = p.split()
+			pointtuple = (output_scale[0]*float(twostrings[0]), output_scale[1]*(height-float(twostrings[1])))	#the y value is inverted in the gnuplot
+			pointlist.append(pointtuple)
+	plot.close()
+
+	print("deleting temporary files...")
+	os.remove("./autotrace_temp/input.bmp")
+	os.remove("./autotrace_temp/output.gnuplot")
+
+	return pointlist
+
+
--- main.py
+++ main.py
@@ -2,15 +2,35 @@ from linemorph import *
 import sys
 
 if (len(sys.argv) < 4):
-    print("expected arguments src1 src2 destname")
+    print("expected arguments: src1 src2 destdir")
     sys.exit()
 
 srca = sys.argv[1]
 srcb = sys.argv[2]
-dest = sys.argv[3]
-a = open_image(srca)
-b = open_image(srcb)
+destdir = sys.argv[3]
+if (destdir[-1] != "/"):
+	destdir = destdir+"/"
 
-apoints = getPointList(a, 320)
-newimage = drawImageFromPoints(apoints)
-save_image(newimage, dest)
+print(srca)
+a_orig = Image.open(srca)
+a_edge = edgedetect(reduceSize(a_orig))
+print(srcb)
+b_orig = Image.open(srcb)
+b_edge = edgedetect(reduceSize(b_orig))
+
+a, b = equalSize(a_orig, b_orig)
+a_p_scale = (a.size[0]/reduce_width, a.size[1]/reduce_height)
+b_p_scale = (b.size[0]/reduce_width, b.size[1]/reduce_height)
+a_p = getPointsFromAutotrace(a_edge, a_p_scale)
+b_p = getPointsFromAutotrace(b_edge, b_p_scale)
+
+a.save(destdir+"a.png")
+b.save(destdir+"b.png")
+
+matches = matchPointLists(a_p, b_p)
+a_d = getDelaunay(a_p)
+b_d = getDelaunay(b_p)
+
+for i in range(0, 11):
+	blend_image = interpolateWithDots(a, b, a_p, b_p, matches, float(i/10))
+	blend_image.save(destdir+str(i)+".png")
